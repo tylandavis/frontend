cf531145a131e484470b29fdbaaad780616fd12f
Tylan Davis <89618855+tylandavis@users.noreply.github.com>
2024-11-17 22:40:05 -0500

    Update page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,14 +8,14 @@ export default async function Page() {
 		<main>
 			<p>Tylan Davis.</p>
 			<p>
-				Designer, developer, <Link href='https://payloadcms.com'>Payload</Link>.
+				Designer, Developer, <Link href='https://payloadcms.com'>Payload</Link>.
 			</p>
 			<p>Grand Rapids, Michigan, USA.</p>
 			<p>
 				<Link href='https://github.com/tylandavis'>GitHub</Link>.{' '}
 				<Link href='https://bsky.app/profile/tylandavis.com'>Bluesky</Link>.{' '}
 				<Link href='https://www.behance.net/tylandavis'>Behance</Link>.{' '}
-				<Link href='mailto:hello@tylandavis'>Email</Link>.
+				<Link href='mailto:hello@tylandavis.com'>Email</Link>.
 			</p>
 			<Image
 				src={braille}
