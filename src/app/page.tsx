80738375f18ab1a0508525591c29adcf6559e33b
Tylan Davis <tylan@payloadcms.com>
2025-06-04 21:07:00 -0600

    i live in Utah now

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default async function Page() {
 			<p>
 				Designer, Developer, <Link href='https://payloadcms.com'>Payload</Link>.
 			</p>
-			<p>Grand Rapids, Michigan, USA.</p>
+			<p>Salt Lake City, Utah, USA.</p>
 			<p>
 				<Link href='https://github.com/tylandavis'>GitHub</Link>.{' '}
 				<Link href='https://bsky.app/profile/tylandavis.com'>Bluesky</Link>.{' '}
